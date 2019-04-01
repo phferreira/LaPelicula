e484c85e096d44fbe7bb16e401bafb84c65c0dbd
phferreira <p.h.ferreirah@gmail.com>
2019-03-31 22:39:24 -0300

    Tarefa: #0002 Merge com os fontes disponibilizados

--- App.js
+++ App.js
@@ -1,29 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import {
+  View, ActivityIndicator, StatusBar,
+  AsyncStorage, StyleSheet
+}
+  from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  createSwitchNavigator,
+  createAppContainer
+}
+  from 'react-navigation';
+import { openDatabase } from 'react-native-sqlite-storage';
+// criando um objeto para representar o banco de dados
+var db = openDatabase({
+  name: 'lapelicula.db'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+import LoginScreen from './src/screen/LoginScreen';
+import ListaFilmesScreen from './src/screen/ListaFilmesScreen';
+import FilmeScreen from './src/screen/FilmeScreen';
+import SobreScreen from './src/screen/SobreScreen';
+import CameraScreen from './src/screen/CameraScreen';
+
+class LoginLoadingScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.bootstrapAsync = this.bootstrapAsync.bind(this);
+
+    this.bootstrapAsync();
+    // criação das tabelas no banco de dados
+    db.transaction(function (tx) {
+      // códigos ...
+      tx.executeSql(
+        'SELECT name FROM sqlite_master WHERE type = \'table\' AND name = \'filme\'',
+        [],
+        function (txp, res) {
+          if (res.rows.length == 0) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS filme(codigo INTEGER PRIMARY KEY AUTOINCREMENT, descricao VARCHAR(200), imagem blob)', []);
+            tx.executeSql('INSERT INTO filme(descricao) VALUES(\'filme 01\')', []);
+            tx.executeSql('INSERT INTO filme(descricao) VALUES(\'filme 02\')', []);
+            tx.executeSql('INSERT INTO filme(descricao) VALUES(\'filme 03\')', []);
+          }
+        });
+    });
+  }
+
+
+  // Fetch the token from storage then navigate to our appropriate place
+  bootstrapAsync() {
+    let logado = 'false';
+    AsyncStorage.getItem('logado').then((value) => {
+      logado = value;
+    });
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    this.props.navigation.navigate(logado == 'true' ? 'App' : 'Auth');
+  };
+
+  // Render any loading content that you like here
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
       </View>
     );
   }
@@ -32,18 +75,47 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    justifyContent: 'center',
   },
 });
+
+
+// criar um navegador Tab
+const AppTab = createBottomTabNavigator({
+  Home: { screen: ListaFilmesScreen },
+  Filme: { screen: FilmeScreen },
+  Sobre: { screen: SobreScreen }
+}, {
+    tabBarOptions: {
+      activeTintColor: '#FFFFFF',
+      inactiveTintColor: '#CCCCCC',
+      style: {
+        backgroundColor: '#570076'
+      },
+      indicatorStyle: {
+        backgroundColor: null
+      }
+    }
+  });
+
+// criando navegador Stack
+const AppStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Camera: CameraScreen
+  }
+);
+
+// criando navegador Switch
+const AppSwitch = createSwitchNavigator({
+  LoginLoading: LoginLoadingScreen,
+  App: AppTab,
+  Auth: AppStack
+}, {
+    initialRouteName: 'LoginLoading'
+  });
+
+// container principal da aplicação
+const App = createAppContainer(AppSwitch);
+export default App;
--- android/app/src/main/java/com/lapelicula/MainApplication.java
+++ android/app/src/main/java/com/lapelicula/MainApplication.java
@@ -3,10 +3,13 @@ package com.lapelicula;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new RNCameraPackage(),
+            new SQLitePluginPackage()
       );
     }
 
